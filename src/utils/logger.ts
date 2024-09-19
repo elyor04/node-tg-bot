a1ba26f8aa5992102efb537a7d5bf919b74e038f
Elyor Tukhtamuratov <tuxtamuratovelyor@gmail.com>
2024-09-20 00:55:57 +0500

    update logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -2,15 +2,15 @@ import formatDate from "./formatDate";
 
 const log = (level: string, message: string) => {
   const timestamp: string = formatDate(new Date());
-  console.log(`[${timestamp}] [${level.toUpperCase()}] ${message}`);
+  console.log(`[${timestamp}] [${level}] ${message}`);
 };
 
 // Logging methods
 const logger = {
-  info: (message: string) => log("info", message),
-  warn: (message: string) => log("warn", message),
-  error: (message: string) => log("error", message),
-  debug: (message: string) => log("debug", message),
+  info: (message: string) => log("INFO", message),
+  warn: (message: string) => log("WARN", message),
+  error: (message: string) => log("ERROR", message),
+  debug: (message: string) => log("DEBUG", message),
 };
 
 export default logger;
