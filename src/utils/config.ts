0db280d4cbdb846794db0f538c79c629dd149265
Elyor Tukhtamuratov <tuxtamuratovelyor@gmail.com>
2024-09-24 14:13:38 +0500

    fix

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,10 +1,10 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-const BOT_TOKEN = process.env.BOT_TOKEN || "";
-const SAP_BASE_URL = process.env.SAP_BASE_URL || "";
-const SAP_LOGIN = process.env.SAP_LOGIN || "";
-const SAP_PASSWORD = process.env.SAP_PASSWORD || "";
-const SAP_COMPANY_DB = process.env.SAP_COMPANY_DB || "";
+const BOT_TOKEN = process.env?.BOT_TOKEN?.replace(/\\\\/g, "\\") || "";
+const SAP_BASE_URL = process.env?.SAP_BASE_URL?.replace(/\\\\/g, "\\") || "";
+const SAP_LOGIN = process.env?.SAP_LOGIN?.replace(/\\\\/g, "\\") || "";
+const SAP_PASSWORD = process.env?.SAP_PASSWORD?.replace(/\\\\/g, "\\") || "";
+const SAP_COMPANY_DB = process.env?.SAP_COMPANY_DB?.replace(/\\\\/g, "\\") || "";
 
 export { BOT_TOKEN, SAP_BASE_URL, SAP_LOGIN, SAP_PASSWORD, SAP_COMPANY_DB };
