fcce009e5c29864790ffdeb2c17fe5d3b158089b
Elyor Tukhtamuratov <tuxtamuratovelyor@gmail.com>
2024-09-25 11:02:24 +0500

    update logMiddleware

--- src/middlewares/log.ts
+++ src/middlewares/log.ts
@@ -3,10 +3,27 @@ import NextFunction from "../types/nextFunction";
 import logger from "../utils/logger";
 
 const logMiddleware = async (ctx: Context, next: NextFunction) => {
-  logger.info(
-    `Received (update_type=${ctx.updateType}, user_id=${ctx.from?.id})`
-  );
-  await next(); // Move to the next middleware or handler
+  if (ctx.chat?.type !== "private") return;
+
+  const user = ctx.from;
+  const user_id = user?.id;
+  const full_name = `${user?.first_name || ""} ${user?.last_name || ""}`.trim();
+  const username = user?.username ? `"@${user.username}"` : null;
+
+  const startTime = Date.now();
+
+  await next()
+    .then(() => {
+      const spentTime = Date.now() - startTime;
+      logger.info(
+        `User id=${user_id}, name="${full_name}", username=${username}. Duration ${spentTime} ms`
+      );
+    })
+    .catch((err) => {
+      const spentTime = Date.now() - startTime;
+      logger.error(`${err?.name} - ${err?.message}. Duration ${spentTime} ms`);
+      console.error(err);
+    });
 };
 
 export default logMiddleware;
